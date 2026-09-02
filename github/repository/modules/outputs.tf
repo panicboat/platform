1d07beacbef8742796e405274177f1e464d04ec7
panicboat <panicboat@gmail.com>
2026-09-03 01:48:11 +0900

    chore: Remove unused aws_cloudwatch_log_group (#868)

--- github/repository/modules/outputs.tf
+++ github/repository/modules/outputs.tf
@@ -27,13 +27,3 @@ output "repository_ssh_clone_urls" {
   description = "URLs for cloning repositories via SSH"
   value       = { for k, v in github_repository.repository : k => v.ssh_clone_url }
 }
-
-output "cloudwatch_log_group_names" {
-  description = "Names of the CloudWatch log groups"
-  value       = { for k, v in aws_cloudwatch_log_group.github_repository_logs : k => v.name }
-}
-
-output "cloudwatch_log_group_arns" {
-  description = "ARNs of the CloudWatch log groups"
-  value       = { for k, v in aws_cloudwatch_log_group.github_repository_logs : k => v.arn }
-}
